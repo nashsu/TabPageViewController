8d2223f8dbe2180fc67ddb15d210ff665afb2610
nash_su <nash.yong@gmail.com>
2018-01-02 17:04:47 +0800

    update

--- Demo/TabPageViewControllerDemo/LimitedTabPageViewController.swift
+++ Demo/TabPageViewControllerDemo/LimitedTabPageViewController.swift
@@ -15,10 +15,25 @@ class LimitedTabPageViewController: TabPageViewController {
         super.init()
         let vc1 = UIViewController()
         vc1.view.backgroundColor = UIColor.white
+        
+        let vc3 = UIViewController()
+        vc3.view.backgroundColor = UIColor.white
+        
+        let vc4 = UIViewController()
+        vc4.view.backgroundColor = UIColor.white
+        
+        let vc5 = UIViewController()
+        vc5.view.backgroundColor = UIColor.white
+        
         let vc2 = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ListViewController")
-        tabItems = [(vc1, "First"), (vc2, "Second")]
-        option.tabWidth = view.frame.width / CGFloat(tabItems.count)
+        tabItems = [(vc1, "推荐"), (vc2, "新闻"), (vc3, "电影"), (vc4, "音乐"), (vc5, "视频")]
+        option.tabWidth = (view.frame.width - 50) / CGFloat(tabItems.count)
         option.hidesTopViewOnSwipeType = .all
+        option.tabBackgroundColor = UIColor(red:0.16, green:0.16, blue:0.16, alpha:1.00)
+        option.currentBarHeight = 0
+        option.tabHeight = 64
+        option.fontSize = 16
+        option.currentColor = UIColor(red:0.35, green:0.78, blue:0.56, alpha:1.00)
     }
 
     required init?(coder aDecoder: NSCoder) {
